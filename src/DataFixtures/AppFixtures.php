a38e19423762cdf4c79f3f9d0796e93c284196a9
Garald <lordgeck@gmail.com>
2021-10-25 15:04:57 +0200

    Updated fixtures with faker (#14)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,36 +4,59 @@ namespace App\DataFixtures;
 
 use App\Entity\Client;
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
-    private $encoder;
-
-    public function __construct(UserPasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
+    public function __construct(
+        private UserPasswordHasherInterface $encoder,
+        private SluggerInterface $slugger,
+    ) {}
 
     public function load(ObjectManager $manager)
     {
-        $client = new Client();
-        $client->setUsername('ClientDemo')
-            ->setPassword($this->encoder->encodePassword($client, 'password'));
-        $manager->persist($client);
+        $faker = Factory::create();
 
-        $client = new Client();
-        $client->setUsername('ClientDemo2')
-            ->setPassword($this->encoder->encodePassword($client, 'password'));
-        $manager->persist($client);
+        $client1 = new Client();
+        $client1->setUsername('ClientDemo')
+            ->setPassword($this->encoder->hashPassword($client1, 'password'));
+        $manager->persist($client1);
+        $client2 = new Client();
+        $client2->setUsername('ClientDemo2')
+            ->setPassword($this->encoder->hashPassword($client2, 'password'));
+        $manager->persist($client2);
 
-        $product = new Product();
-        $product->setName('ProductDemo')
-            ->setPrice('149.99')
-            ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent suscipit, tellus id imperdiet egestas, diam risus mattis metus, vitae lobortis ex libero et enim. Curabitur pretium libero dolor. Donec commodo diam eget justo efficitur, nec placerat neque auctor. Maecenas vestibulum nisl et lectus lacinia, eu mollis ligula congue. In finibus cursus finibus. Integer viverra at arcu vel pulvinar. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec dignissim nulla et lorem condimentum, eu cursus sapien tempor.');
-        $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setUsername($faker->unique()->userName())
+                ->setSlug($this->slugger->slug($user->getUsername()))
+                ->setEmail($faker->unique()->email())
+                ->setClient($client1);
+            $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setUsername($faker->unique()->userName())
+                ->setSlug($this->slugger->slug($user->getUsername()))
+                ->setEmail($faker->unique()->email())
+                ->setClient($client2);
+            $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 40; $i++) {
+            $product = new Product();
+            $product->setName($faker->unique()->word())
+                ->setSlug($this->slugger->slug($product->getName()))
+                ->setPrice($faker->randomFloat(2, 50, 1000))
+                ->setDescription($faker->text(250));
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
